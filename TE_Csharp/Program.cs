59668fb56d887542bab0118bcb35a4aa8c8bc5ab
AshithaKulal <ashithakadthala@gmail.com>
2022-09-22 16:09:49 +0530

    Add project files.

---
+++ TE_Csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace basic
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome C Sharp");
+        }
+    }
+}
