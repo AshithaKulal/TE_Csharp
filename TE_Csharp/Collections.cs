e68cf70fe1ad7ff472d543ebf662cd0e27c6d6c1
AshithaKulal <ashithakadthala@gmail.com>
2022-10-04 17:31:51 +0530

    commit 04/10

---
+++ TE_Csharp/Collections.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_Csharp
+{
+    internal class Collections
+    {
+        public static void Dictionary()
+        {
+            //Specify the type of key value
+            Dictionary<int, string> vs = new Dictionary<int, string>();
+            vs.Add(1, "C#");
+            vs.Add(2, "SQL");
+            vs.Add(3, "Angular");
+            vs.Add(4, ".NET");
+            Console.WriteLine("DICTIONARY");
+            //display from key 1 to 4
+            foreach (var obj in vs)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+            var values = new List<string>(vs.Values);
+            foreach (var obj in values)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Key:1 {vs[1]}");
+        }
+
+        public static void Hashtable()
+        {
+            //Not type specific
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "abc");
+            ht.Add(2, 123);
+            ht.Add(3, "zy98");
+            ht.Add(4, 21.5);          
+            Console.WriteLine("HASHTABLE");
+            //display in reverse order key 4 to 1
+            ICollection key = ht.Keys;
+            foreach (var obj in key)
+            {
+                Console.WriteLine(ht[obj]);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Key:1 {ht[1]}");
+            Console.WriteLine($"Key:4 {ht[4]}");
+        }
+
+        public static void Main()
+        {
+            Hashtable();
+            Console.WriteLine("-----------------------------------------");
+            Dictionary();
+        }
+    }
+}
---
+++ TE_Csharp/Generics.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_Csharp
+{
+    internal class Generics
+    {
+        public delegate T AreaDelegate <T> (T value);
+
+        class Area
+        {
+            public int AreaSquare(int side)
+            {
+                return (side * side);
+            }
+            public double AreaCircle(double radius)
+            {
+                return (radius * radius * 3.14);
+            }
+            public int AreaCube(int side)
+            {
+                return (6 * side * side);
+            }
+        }
+        public static void Main()
+        {
+            Area area = new Area();
+            AreaDelegate<int> d = new AreaDelegate<int>(area.AreaSquare);
+
+            Console.WriteLine("Enter the side of square");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of square:"+ d(s));
+            Console.WriteLine("-------------------------------");
+
+            d=area.AreaCube;
+            Console.WriteLine("Enter the side of cube");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of square:" + d(a));
+            Console.WriteLine("-------------------------------");
+
+            AreaDelegate<double> d1 = new AreaDelegate<double>(area.AreaCircle);
+            Console.WriteLine("Enter the radius of circle");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of square:" + d1(r));
+            Console.WriteLine("-------------------------------");
+        }
+
+       
+       
+        
+    }
+
+}
